32ee180c9873607d940109384eee27d7346888ff
Tricia Holmes <tricia.michele.holmes@gmail.com>
2022-12-10 10:02:27 -0600

    Add types and props for Input keydown events

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import { useState } from "react";
+import { useState, KeyboardEvent} from "react";
 import "./App.css";
 import Button from "./components/Button/Button";
 import Promopt from "./components/Prompt/Prompt";
 import Input from "./components/Input/Input";
 
 function App() {
+  const [text, setText] = useState("");
+
+  const handleChange = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === " " || e.key === "Space") {
+      console.log("YOU DID IT");
+    }
+    // console.log(event.target.value);
+  };
+
   return (
     <div className="App">
       <Promopt words="I am just going to add a bunch of words and see what happens" />
-      <Input className="game__input" />
+      <Input className="game__input" onKeyDown={handleChange} value={text} />
       <Button className="game__btn" content="See the top scores!" />
     </div>
   );
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import { useState, KeyboardEvent } from "react";
 
 type InputProps = {
   className: string;
+  onKeyDown: (e: KeyboardEvent<HTMLInputElement>) => void;
+  value: string;
 };
 
-export default function Input({ className }: InputProps) {
-  const [inputValue, setInputValue] = useState("");
-
+export default function Input({ className, onKeyDown }: InputProps) {
   return (
-    <form>
-      <input className={className}></input>
-    </form>
+    <div>
+      <input type="text" name="text" className={className} onKeyDown={onKeyDown}></input>
+    </div>
   );
 }
