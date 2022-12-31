f9b41f1bd4c9e5df15023763b1119c57f37481a2
Tricia Holmes <tricia.michele.holmes@gmail.com>
2022-12-31 01:42:31 -0600

    Remove unused code

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import './assets/fonts/PressStart2P-Regular.ttf'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
